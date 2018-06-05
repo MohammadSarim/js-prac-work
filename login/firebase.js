c6a044c4849c798270e719d200b434fe66f7a1ff
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-06-05 23:07:01 +0500

    added

---
+++ login/firebase.js
@@ -0,0 +1,19 @@
+var app_firebase = {}
+(function (){
+    
+<script src="https://www.gstatic.com/firebasejs/5.0.2/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAnxID9LwOoZKJBqM-Mmo_obB6FXWf1xDE",
+    authDomain: "stop-watch-3ea94.firebaseapp.com",
+    databaseURL: "https://stop-watch-3ea94.firebaseio.com",
+    projectId: "stop-watch-3ea94",
+    storageBucket: "stop-watch-3ea94.appspot.com",
+    messagingSenderId: "565444832656"
+  };
+  firebase.initializeApp(config);
+
+  app_firebase = firebase;
+})
+

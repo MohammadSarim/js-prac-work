40b3e0d53438aef749653c6cfd0d7c96b882a952
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-23 20:56:36 +0500

    added work

--- digital_clock/js/index.js
+++ digital_clock/js/index.js
@@ -1,1 +1,23 @@
-console.log("siccess");
+var content = document.querySelector(".content");
+content.addEventListener("load" , todayTime());
+
+// ==================================================== EventListener ====================================================
+
+
+function todayTime(){
+let time = new Date();
+let hours = time.getHours();
+let min = time.getMinutes();
+let sec = time.getSeconds();
+// console.log(sec);
+// min = checkTime(min);
+sec = checkTime(sec);
+content.innerHTML = hours + ":" + min + ":" + sec ;
+var t = setTimeout(todayTime, 500); 
+}
+
+function checkTime(i) {
+    // console.log(i);
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
